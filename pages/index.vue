d13b93563c0326b4304ded38324057a8740dd070
gcharang <mrgcharang@gmail.com>
2020-03-20 20:05:50 +0530

    initial commit; can get data and display it

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <article>
+    <section>
+      <h1>Notary Stats 2020</h1>
+      <div>
+        <li v-for="notary in notaries" class="item">{{ notary.name }}</li>
+      </div>
+    </section>
+  </article>
+</template>
+
+<script>
+export default {
+  data() {
+    return { notaries: {} };
+  },
+  async asyncData({ $axios }) {
+    const notaries = await $axios.$get(
+      "https://kmd-data.s3.us-east-2.amazonaws.com/notary-stats-2020/main.json"
+    );
+    return { notaries: notaries };
+  }
+};
+</script>
+
+<style>
+</style> 
