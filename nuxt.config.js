de0bac3865fe61f72afbe548096e7af284c17076
gcharang <mrgcharang@gmail.com>
2020-03-21 19:56:11 +0530

    trying to correct spa mode

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,17 @@
+// only add `router.base = '/<repository-name>/'` if `DEPLOY_ENV` is `GH_PAGES`
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+    router: {
+        base: '/notary-testnet-stats-2020/'
+    }
+} : {}
+
+
 module.exports = {
     modules: [
         '@nuxtjs/axios',
     ],
     buildModules: ['@nuxtjs/vuetify'],
     mode: "spa",
-    base: "/notary-testnet-stats-2020/"
+    ...routerBase
+
 }
