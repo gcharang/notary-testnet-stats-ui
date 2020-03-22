e696be29b38676720d2e9fc14557ff05eaa1d80f
gcharang <mrgcharang@gmail.com>
2020-03-22 20:38:15 +0530

    commit before trial and error to get nuxt and vuetify to behave well together

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,43 @@
-
 <template>
   <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app clipped expand-on-hover>
+      <v-list>
+        <v-list-item v-for="item in navDrawerItems" :key="item.title" link nuxt>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-app-bar app clipped-left>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title>Notary Testnet Stats 2020</v-toolbar-title>
     </v-app-bar>
 
     <v-content>
       <v-container class="fill-height" fluid>
-        <v-data-table
-          :headers="headers"
-          :items="notaries"
-          :sort-by="['total']"
-          :sort-desc="[true]"
-          multi-sort
-          item-key="name"
-          class="elevation-1"
-        ></v-data-table>
+        <v-card>
+          <v-card-title>All Time</v-card-title>
+          <v-data-table
+            :headers="headers"
+            :items="notaries"
+            :sort-by="['total']"
+            :sort-desc="[true]"
+            multi-sort
+            item-key="name"
+            class="elevation-1"
+          ></v-data-table>
+        </v-card>
       </v-container>
     </v-content>
 
     <v-footer app>
-      <span>&copy; gcharang 2020</span>
+      <span>&copy; 2020</span>
     </v-footer>
   </v-app>
 </template>
@@ -33,6 +50,12 @@ export default {
   data() {
     return {
       notaries: {},
+      navDrawerItems: [
+        { title: "Total Stats", icon: "mdi-view-dashboard" },
+        { title: "Past Day", icon: "mdi-numeric-1-box" },
+        { title: "Past Three Days", icon: "mdi-numeric-3-box" },
+        { title: "Past Week", icon: "mdi-numeric-7-box" }
+      ],
       headers: [
         {
           text: "Name",
@@ -49,7 +72,7 @@ export default {
         { text: "TXSCLAPOW.total", value: "TXSCLAPOW.totalNotas" },
         { text: "TXSCLAPOW.lastNota", value: "TXSCLAPOW.timeSinceLastNota" }
       ],
-      drawer: null
+      drawer: true
     };
   },
   created() {
